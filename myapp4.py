464790b7b9eae171c185e35a67ac3444cfe8c386
AbhinavSivanandhan <abhinavstar.anand@gmail.com>
2022-03-17 12:17:17 +0530

    server basic table

--- myapp4.py
+++ myapp4.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from sfdclib import SfdcSession
 from simple_salesforce import Salesforce
 from sfdclib import SfdcToolingApi
@@ -7,7 +6,10 @@ import csv
 import time
 import urllib
 from pivottablejs import pivot_ui
+import pandas as pd
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
 ########################
 sf = Salesforce(username='abhinav.sivanandhan@mindful-raccoon-e14i2o.com',password='abcde12345', security_token='vGoaEvzwHd2KZThFCFoIBrQDa')
 data = sf.query_all_iter("SELECT Id, Action, CreatedBy.Name, CreatedDate, Display, Section FROM SetupAuditTrail WHERE CreatedDate=THIS_MONTH")
@@ -17,12 +19,13 @@ for row in data:
     for key, value in row.items():
         df[key]=value
 print(df)
-pivot_ui(df,outfile_path=’pivottablejs.html’)
-HTML(‘pivottablejs.html’)
+pivot_ui(df,outfile_path='pivottablejs.html')
+#https://towardsdatascience.com/two-essential-pandas-add-ons-499c1c9b65de
 ########################
 @app.route("/")
 def hello_world():
-    return "<p>#### {%   %}  ####</p>"
+    return render_template("index.html")
+    #return HTML('pivottablejs.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- myapp5.py
+++ myapp5.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from sfdclib import SfdcSession
 from simple_salesforce import Salesforce
 from sfdclib import SfdcToolingApi
@@ -7,22 +6,28 @@ import csv
 import time
 import urllib
 from pivottablejs import pivot_ui
+import pandas as pd
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
 ########################
 sf = Salesforce(username='abhinav.sivanandhan@mindful-raccoon-e14i2o.com',password='abcde12345', security_token='vGoaEvzwHd2KZThFCFoIBrQDa')
-data = sf.query_all_iter("SELECT Id, Action, CreatedBy.Name, CreatedDate, Display, Section FROM SetupAuditTrail WHERE CreatedDate=THIS_MONTH")
+data = sf.query_all_iter("SELECT Id, Action, CreatedBy.Name, CreatedDate, Display, Section FROM SetupAuditTrail WHERE CreatedDate=THIS_YEAR")
 COLUMN_NAMES=['Id','Action','CreatedBy','Display','Delegate User']
 df = pd.DataFrame(columns=COLUMN_NAMES)
 for row in data:
+    print(row)
     for key, value in row.items():
         df[key]=value
 print(df)
-pivot_ui(df,outfile_path=’pivottablejs.html’)
-HTML(‘pivottablejs.html’)
+#pivot_ui(df,outfile_path='pivottablejs.html')
+#https://towardsdatascience.com/two-essential-pandas-add-ons-499c1c9b65de
 ########################
 @app.route("/")
 def hello_world():
-    return "<p>#### {%   %}  ####</p>"
+    return render_template("index.html", column_names=df.columns.values, row_data=list(df.values.tolist()),
+                           link_column="Id", zip=zip)
+    #return HTML('pivottablejs.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
